6341de480cbb7670d753e0cbf34d237a76952116
Yakov Didenko <kogotto@gmail.com>
2023-08-02 16:22:24 +0400

    Use 'gr' for telescope lsp_references if can

--- nvim-lua-config/lua/kogo/plugins/lsp/lspconfig.lua
+++ nvim-lua-config/lua/kogo/plugins/lsp/lspconfig.lua
@@ -13,6 +13,8 @@ if not cmpNvimLspStatus then
     return
 end
 
+local telescopeBuiltinStatus, telescopeBuiltin = pcall(require, 'telescope.builtin')
+
 local keymap = vim.keymap
 
 -- This adds lsp related keymaps only for buffers with existing lsp server
@@ -27,7 +29,11 @@ local onAttach = function(client, buf)
     keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     keymap.set('n', 'gD', '<cmd>Lspsaga peek_definition<CR>', opts)
     keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    if telescopeBuiltinStatus then
+        keymap.set('n', 'gr', telescopeBuiltin.lsp_references, opts)
+    else
+        keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    end
     keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<CR>', opts)
     keymap.set('n', '<leader>rn', '<cmd>Lspsaga rename<CR>', opts)
     keymap.set('n', '<leader>D', '<cmd>Lspsaga show_line_diagnostics<CR>', opts)
