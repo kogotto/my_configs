28185c631ed6d21880ec2a7c8d503502a211edc3
Yakov Didenko <kogotto@gmail.com>
2026-04-14 14:49:30 +0300

    Add installation zsh

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ mkdir -p ~/.config/nvim
 ln -s $(realpath nvim-lua-config/init.lua) ~/.config/nvim/
 ln -s $(realpath nvim-lua-config/lua) ~/.config/nvim/
 
+sudo apt install -y zsh
+ln -s $(realpath .zshrc) ~/
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s $(realpath .tmux.conf) ~/
 
